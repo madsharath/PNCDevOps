ec586e958af74913515192208b3ba6e1417f7faa
PDC4-Training.PDC4 <PDC4-Training.PDC4@P4A-508SD5Y.dir.svc.accenture.com>
2018-08-02 11:02:47 +0530

    changes plugin=json

--- src/test/java/org/example/PNCDevOps/RunCukesTest.java
+++ src/test/java/org/example/PNCDevOps/RunCukesTest.java
@@ -6,6 +6,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 //@CucumberOptions(plugin = {"pretty"})
-@CucumberOptions(features="C:\\JogaDevOps\\PNCDevOps\\Resources\\LoginTest.feature")
+@CucumberOptions(plugin={"json"})
 public class RunCukesTest {
 }
